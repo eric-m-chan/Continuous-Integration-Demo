4ba295e1632eb9afac63fe23d4083cf4b6bfada7
eric-m-chan <echan044@ucr.edu>
2026-05-06 10:21:45 -0700

    trying demo test

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,6 @@ int main() {
 	std::cout << "Hello World!\n";
 	std::cout << "how are you?\n";
 	std::cout << "I'm doing well, my name is Alice\n";
+	std::cout << "we are on demo test\n";
 	return 0;
 }
